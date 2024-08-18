631da4f243eb33236f11777bfe5b1af8acf731f5
Anshul Gour <159023836+Anshul-gour01@users.noreply.github.com>
2024-08-18 19:08:31 +0530

    Add files via upload

---
+++ railres/firstimport.php
@@ -0,0 +1,14 @@
+<?php
+$host="localhost"; // Host name
+$username="root"; // Mysql username
+$password=""; // Mysql password
+$db_name="railres"; // Database name
+
+$conn=mysqli_connect("$host", "$username", "$password","$db_name");
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  echo "Connected successfully";
+
+?>
