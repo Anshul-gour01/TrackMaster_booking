72d14ba3de3680e22509df8ca5a3451b671c3012
Anshul Gour <159023836+Anshul-gour01@users.noreply.github.com>
2024-10-09 22:47:24 +0530

    Update login1.php

--- railres/login1.php
+++ railres/login1.php
@@ -118,15 +118,8 @@ session_start();
 		<br/>
 		<!-- Footer -->
 <footer >
-		<div style="width:100%;">
-			<div style="float:left;">
-			<p class="text-right text-info">  &copy; 2018 Copyright PVT Ltd.</p>	
-			</div>
-			<div style="float:right;">
-			<p class="text-right text-info">	Desinged By : projectworlds</p>
-			</div>
-		</div>
-		</footer>		
+		
+</footer>		
 	</div>
 </body>
 </html>
