d11e2646d2424dae020f56d2de2ade45392aec52
Anshul Gour <159023836+Anshul-gour01@users.noreply.github.com>
2024-10-09 22:46:43 +0530

    Update index.php

--- railres/index.php
+++ railres/index.php
@@ -107,10 +107,10 @@ session_start();
 <footer >
 		<div style="width:100%;">
 			<div style="float:left;">
-			<p class="text-right text-info">  &copy; 2018 Copyright PVT Ltd.</p>	
+			<p class="text-right text-info"></p>	
 			</div>
 			<div style="float:right;">
-			<p class="text-right text-info">	Desinged By : projectworlds</p>
+			<p class="text-right text-info"></p>
 			</div>
 		</div>
 		</footer>	</div>
